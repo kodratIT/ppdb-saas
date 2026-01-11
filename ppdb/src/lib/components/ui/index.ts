75edff2bc5f94183acbb5caa7bb33a7c48e3e4c7
kodratIT <kodratcoc@gmail.com>
2026-01-11 09:59:08 +0700

    feat(ui): export Modal and Dialog from index

--- ppdb/src/lib/components/ui/index.ts
+++ ppdb/src/lib/components/ui/index.ts
@@ -1,3 +1,5 @@
+export { default as Modal } from './modal.svelte';
+export { default as Dialog } from './dialog.svelte';
 export { default as Button } from './button.svelte';
 export { default as Card } from './card.svelte';
 export { default as CardContent } from './card-content.svelte';
