d1a7da4d4a6499fb26945d8a526337405f5dd048
kodratIT <kodratcoc@gmail.com>
2026-01-18 11:00:45 +0700

    feat(ui): add radio-group component

---
+++ ppdb/src/lib/components/ui/radio-group-item.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { RadioGroup as RadioGroupPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	type $$Props = RadioGroupPrimitive.ItemProps;
+	type $$Events = RadioGroupPrimitive.ItemEvents;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+	export let value: $$Props['value'];
+</script>
+
+<RadioGroupPrimitive.Item
+	{value}
+	class={cn(
+		'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+		className
+	)}
+	{...$$restProps}
+	let:checked
+	on:click
+>
+	<div class="flex items-center justify-center">
+		<RadioGroupPrimitive.ItemIndicator>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="h-2.5 w-2.5 fill-current text-current"
+			>
+				<circle cx="12" cy="12" r="10" />
+			</svg>
+		</RadioGroupPrimitive.ItemIndicator>
+	</div>
+	<RadioGroupPrimitive.ItemInput />
+</RadioGroupPrimitive.Item>
---
+++ ppdb/src/lib/components/ui/radio-group.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { RadioGroup as RadioGroupPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	type $$Props = RadioGroupPrimitive.Props;
+	type $$Events = RadioGroupPrimitive.Events;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+	export let value: $$Props['value'] = undefined;
+	export let onValueChange: $$Props['onValueChange'] = undefined;
+</script>
+
+<RadioGroupPrimitive.Root
+	bind:value
+	{onValueChange}
+	class={cn('grid gap-2', className)}
+	{...$$restProps}
+	on:keydown
+>
+	<slot />
+</RadioGroupPrimitive.Root>
